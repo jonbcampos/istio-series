98c5577703a38d751d7829261ae3784c4b01cdad
Jonathan Campos <jonbcampos@gmail.com>
2018-08-31 11:34:52 -0500

    missed a cd

--- getting-started/scripts/add_bookinfo_version_routing.sh
+++ getting-started/scripts/add_bookinfo_version_routing.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "change directory"
+export ISTIO_VERSION=1.0.0
+cd istio-${ISTIO_VERSION}/
+
 echo "enable mutual TLS destination rules"
 kubectl apply -f samples/bookinfo/networking/destination-rule-all-mtls.yaml
 

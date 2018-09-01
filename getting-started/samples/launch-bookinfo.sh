2e4e98e6705e180356228b2d4495c9baef6b84e7
Jonathan Campos <jonbcampos@gmail.com>
2018-09-01 13:44:12 -0500

    fixing docs

--- getting-started/samples/launch-bookinfo.sh
+++ getting-started/samples/launch-bookinfo.sh
@@ -18,7 +18,7 @@ sleep 10
 sh check-bookinfo-gateway.sh
 
 echo "if this errors out you can always retry by calling:"
-echo "sh ~/istio-series/getting-started/samples/check_bookinfo_gateway.sh"
+echo "sh ~/istio-series/getting-started/samples/check-bookinfo-gateway.sh"
 
 echo "you'll also want to add in the version routing for the samples:"
 echo "sh ~/istio-series/getting-started/samples/add-bookinfo-version-routing.sh"
